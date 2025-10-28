2c1935bd8c825d456a54859b508392c97cc6dbd9
Douglas Saturnino <douglasaturnino@gmail.com>
2025-10-28 15:44:56 -0300

    refactor: Altera para a utilização da função init_chat_model

--- app/ingest/embed_qdrant.py
+++ app/ingest/embed_qdrant.py
@@ -1,23 +1,28 @@
-from qdrant_client import QdrantClient
-from app.utils.settings import settings
-from langchain_openai import OpenAIEmbeddings, ChatOpenAI
+from langchain.chat_models import init_chat_model
+from langchain_community.embeddings.fastembed import FastEmbedEmbeddings
 from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
+
+from app.utils.settings import settings
 
 
 class EmbeddingSelfQuery:
     def __init__(self) -> None:
-        self.llm = ChatOpenAI(model="gpt-4.1-mini", temperature=0)
+        self.llm = init_chat_model(
+            model=settings.MODEL_NAME,
+            temperature=settings.TEMPERATURE,
+        )
         self.client = QdrantClient(
             host=settings.QDRANT_HOST,
             port=settings.QDRANT_PORT,
             timeout=120,
         )
 
-        self.model = OpenAIEmbeddings(
-            model="text-embedding-3-large",
-        )
+        self.model = FastEmbedEmbeddings(model_name=settings.EMBEDDINGS_NAME)
 
-    def get_qdrant_vector_store(self, collection_name: str) -> QdrantVectorStore:
+    def get_qdrant_vector_store(
+        self, collection_name: str
+    ) -> QdrantVectorStore:
         return QdrantVectorStore(
             client=self.client,
             collection_name=collection_name,
--- app/utils/settings.py
+++ app/utils/settings.py
@@ -1,6 +1,7 @@
 import os
 from platform import system
-from dotenv import load_dotenv, find_dotenv
+
+from dotenv import find_dotenv, load_dotenv
 
 
 class Settings:
@@ -9,8 +10,12 @@ class Settings:
     ENV_FILE = find_dotenv()
     SYSTEM = system()
 
-    QDRANT_HOST = "localhost"
-    QDRANT_PORT = "6333"
+    QDRANT_HOST = os.getenv("QDRANT_HOST")
+    QDRANT_PORT = os.getenv("QDRANT_PORT")
+    TEMPERATURE = os.getenv("TEMPERATURE")
+    GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
+    MODEL_NAME = os.getenv("MODEL_NAME")
+    EMBEDDINGS_NAME = os.getenv("EMBEDDINGS_NAME")
 
 
 settings = Settings()
